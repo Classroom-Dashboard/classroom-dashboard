3dba23a4984fbfec54255b51d657a350da05e646
monaviaco <monicaacosta2015@gmail.com>
2018-02-23 14:49:42 -0800

    Added functions and params to student Controller

--- classroom-dashboard/app/controllers/students_controller.rb
+++ classroom-dashboard/app/controllers/students_controller.rb
@@ -1,4 +1,19 @@
 class StudentsController < ApplicationController
   def index
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+  end
+
+  private
+
+    def student_params
+      params.require(:student).permit(:firstName, :lastName, :classes, :info)
+    end
+
 end
--- classroom-dashboard/db/migrate/20180223182736_create_students.rb
+++ classroom-dashboard/db/migrate/20180223182736_create_students.rb
@@ -1,7 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
-      t.string :name
+      t.string :firstName
+      t.string :lastName
+      t.string :classes
       t.string :info
 
       t.timestamps
