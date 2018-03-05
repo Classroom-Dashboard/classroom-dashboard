926eb2371f82abc523e573a0fad056759fff1139
monaviaco <monicaacosta2015@gmail.com>
2018-03-05 00:28:46 -0800

    Fixed centered student names mushing

--- classroom-dashboard/app/models/student.rb
+++ classroom-dashboard/app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :section
-  has_attached_file :student_img, styles: { student_img: "250x350>", student_index: "325x450>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :student_img, styles: { student_index: "250x350>", student_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :student_img, content_type: /\Aimage\/.*\z/
 end
