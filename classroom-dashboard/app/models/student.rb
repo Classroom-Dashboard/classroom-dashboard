788161e353f068eb05eca4f0aa99fb402c260577
Diana <arreoladiana11@gmail.com>
2018-03-17 14:24:56 -0700

    added file upload option with carrierwave under add student

--- classroom-dashboard/app/models/student.rb
+++ classroom-dashboard/app/models/student.rb
@@ -2,4 +2,6 @@ class Student < ApplicationRecord
   belongs_to :section
   has_attached_file :student_img, styles: { student_index: "250x350>", student_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :student_img, content_type: /\Aimage\/.*\z/
+
+  mount_uploader :image, StudentImageUploader
 end
