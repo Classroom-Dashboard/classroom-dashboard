aad6a3c33d1a273956738ed5285d97f6279ce7ef
Diana <arreoladiana11@gmail.com>
2018-04-18 01:40:16 -0700

    added comments

--- classroom-dashboard/app/models/student.rb
+++ classroom-dashboard/app/models/student.rb
@@ -5,6 +5,7 @@ class Student < ApplicationRecord
   has_attached_file :student_img,styles: {student_index:  "300x300#", thumb: "32x32#", student_show:  "100x100#", student_memory: "600x600#"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :student_img, content_type: /\Aimage\/.*\z/
   
+  # resizes all images prior to the scaling change to the correct dimensions
   Student.all.each {|s| s.student_img.reprocess! }
 
 end
